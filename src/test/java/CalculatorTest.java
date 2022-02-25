06f4442ae0665a299c3a51ae72cf139a2bd5ad4e
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-02-24 21:20:47 -0800

    Appium Testing

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,38 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
+
+public class CalculatorTest {
+
+    static AppiumDriver driver;
+
+    public static void main(String[] args) {
+        try {
+            openCalculator();
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    public static void openCalculator() throws Exception {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("deviceName","Samsung");
+        cap.setCapability("state","device");
+        cap.setCapability("udid","emulator-5554");
+        cap.setCapability("platformName","Android");
+        cap.setCapability("automationName","UiAutomator1");
+        cap.setCapability("platformVersion","9");
+        cap.setCapability("appPackage","com.android.calculator2");
+        cap.setCapability("appActivity","com.android.calculator2.Calculator");
+
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AppiumDriver<MobileElement>(url,cap);
+
+        System.out.println("Application Started...");
+
+    }
+}
+
